ccf7323446396ee7555d7f5781981720bb4b155b
alacaba <alacaba@fastmail.com>
2019-01-11 19:05:06 +0800

    Bootstrap rspec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,17 @@
 require "bundler/setup"
+require 'dotenv'
 require "halo"
+require "vcr"
+require "webmock/rspec"
+
+Dotenv.load
+
+VCR.configure do |config|
+  config.filter_sensitive_data('api-key') { ENV['API_KEY'] }
+  config.cassette_library_dir = "spec/fixtures/cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
